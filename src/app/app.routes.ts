f1768382f41458ca3e9df1b0eafd4331a87fc386
DSI <vitoriagcsantana@gmail.com>
2024-03-13 17:07:09 -0300

    criado o formulário de estado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { EstadoListComponent } from './components/estado/estado-list/estado-list.component';
+import { EstadoFormComponent } from './components/estado/estado-form/estado-form.component';
 
 export const routes: Routes = [
-    { path: 'estados', component: EstadoListComponent, title: 'Lista de Estados'}
+    { path: 'estados', component: EstadoListComponent, title: 'Lista de Estados'},
+    { path: 'estados/new', component: EstadoFormComponent, title: 'Novo Estado'}
 ];
